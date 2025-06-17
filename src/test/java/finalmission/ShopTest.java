a6390cc75d6b0c077133917ce225d4c841a1c238
songsunkook <songsunkook@gmail.com>
2025-06-17 14:39:31 +0900

    test: 예약, 예약 취소, 내 예약 조회

--- src/test/java/finalmission/ShopTest.java
+++ src/test/java/finalmission/ShopTest.java
@@ -1,10 +1,14 @@
 package finalmission;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,8 +20,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import finalmission.shop.dto.ReservationResponse;
 import finalmission.shop.dto.ShopResponse;
-import finalmission.shop.repository.ShopRepository;
+import finalmission.shop.repository.ReservationRepository;
+import finalmission.user.dto.UserRequest;
+import finalmission.user.dto.UserResponse;
 
 @SpringBootTest
 @DirtiesContext
@@ -29,10 +36,29 @@ class ShopTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private ShopRepository shopRepository;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private ReservationRepository reservationRepository;
+
+    private String token;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        // given
+        UserRequest.Login request = new UserRequest.Login("meow@example.com", "1234");
+
+        // when
+        String responseBody = mockMvc.perform(post("/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        UserResponse.Login response = objectMapper.readValue(responseBody, UserResponse.Login.class);
+        token = response.token();
+    }
 
     @Test
     void getAll() throws Exception {
@@ -84,5 +110,82 @@ class ShopTest {
 
         // then
         assertThat(response[0]).isEqualTo(LocalTime.of(12, 0));
+    }
+
+    @Test
+    void reserve() throws Exception {
+        // given
+        Long shopId = 1L;
+
+        // when
+        String responseBody = mockMvc.perform(post("/shops/" + shopId)
+                        .header("Authorization", token)
+                        .param("date", "2025-06-10")
+                        .param("time", "12:00")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var response = objectMapper.readValue(responseBody, ReservationResponse.Created.class);
+
+        // then
+        assertThat(response.shop().id()).isEqualTo(shopId);
+        assertThat(response.date()).isEqualTo(LocalDate.of(2025, 6, 10));
+        assertThat(response.time()).isEqualTo(LocalTime.of(12, 0));
+    }
+
+    @Test
+    void cancel() throws Exception {
+        // given
+        Long shopId = 1L;
+        Long reservationId = 1L;
+        mockMvc.perform(post("/shops/" + shopId)
+                        .header("Authorization", token)
+                        .param("date", "2025-06-10")
+                        .param("time", "12:00")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+        assertThat(reservationRepository.findById(reservationId)).isNotEmpty();
+
+        // when
+        mockMvc.perform(delete("/shops/" + shopId)
+                        .header("Authorization", token)
+                        .param("reservationId", reservationId.toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        // then
+        assertThat(reservationRepository.findById(reservationId)).isEmpty();
+    }
+
+    @Test
+    void myReservations() throws Exception {
+        // given
+        Long shopId = 1L;
+        Long reservationId = 1L;
+        mockMvc.perform(post("/shops/" + shopId)
+                        .header("Authorization", token)
+                        .param("date", "2025-06-10")
+                        .param("time", "12:00")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+        assertThat(reservationRepository.findById(reservationId)).isNotEmpty();
+
+        // when
+        String responseBody = mockMvc.perform(get("/shops/mine")
+                        .header("Authorization", token)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var response = objectMapper.readValue(responseBody, ReservationResponse.Simple[].class);
+
+        // then
+        assertThat(response[0].shop().id()).isEqualTo(shopId);
+        assertThat(response[0].id()).isEqualTo(reservationId);
+        assertThat(response[0].date()).isEqualTo(LocalDate.of(2025, 6, 10));
+        assertThat(response[0].time()).isEqualTo(LocalTime.of(12, 0));
     }
 }
